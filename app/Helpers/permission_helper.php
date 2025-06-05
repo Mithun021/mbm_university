829998160ab352c7c0368ba4cd8b5ae57e679baa
Mithun021 <mithunkr79038@gmail.com>
2025-06-05 16:33:05 +0530

    first commit

--- app/Helpers/permission_helper.php
+++ app/Helpers/permission_helper.php
@@ -8,29 +8,35 @@ use App\Models\Roles_model;
 use CodeIgniter\Modules\Modules;
 function has_permission($user_id, $permission_name)
 {
+    // Load necessary models
     $employee_model = new Employee_model();
+    $roles_model = new Roles_model();
+    $module_roles_model = new Module_roles_model();
+
+    // Get user info
     $user = $employee_model->find($user_id);
 
-    // Admin gets all access
-    if ($user && $user['authority'] === 'admin') {
+    // Admin has full access
+    if ($user && isset($user['authority']) && $user['authority'] === 'admin') {
         return true;
     }
 
-    $modules_model = new Roles_model();
-    $module_category_model = new Module_category_model();
-    $module_roles_model = new Module_roles_model();
-    $permission_model = new Permission_model();
+    // Find module permission by type
+    $module_permission = $roles_model->where('type', $permission_name)->first();
 
+    // If the permission type doesn't exist, deny access
+    if (!$module_permission || !isset($module_permission['id'])) {
+        return false;
+    }
 
-    $module_premission = $modules_model->where('type', $permission_name)->first();
-    // return $module_premission ? true : false;
-
-
-    return $module_roles_model->where('employee_id', $user_id)
-        ->where('module_id', $module_premission['id'])
+    // Check if user has active permission for this module
+    $has_permission = $module_roles_model->where('employee_id', $user_id)
+        ->where('module_id', $module_permission['id'])
         ->where('status', 1)
-        ->first() ? true : false;
+        ->first();
 
+    return $has_permission ? true : false;
 }
+
 
 ?>
